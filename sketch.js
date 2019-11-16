ec0e0232e8e098ad3fa9609839c3780161dcd1ab
Ian Kersey <kerseyi@gmail.com>
2019-11-16 14:14:47 -0800

    so many things

--- sketch.js
+++ sketch.js
@@ -1,29 +1,18 @@
 
 
 var database;
-
 var drawing = [];
-var currentPath = [];
 var isDrawing = false;
 
 function setup() {
   canvas = createCanvas(500, 500);
-
-
-
   canvas.mousePressed(startPath);
   canvas.parent('canvascontainer');
   canvas.mouseReleased(endPath);
-
   var saveButton = select('#saveButton');
   saveButton.mousePressed(saveDrawing);
-
-  var clearButton = select('#clearButton');
-  clearButton.mousePressed(clearDrawing);
-
-
-
-  var config = {
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
     apiKey: "AIzaSyCHpEE0BJ4VJeozU7pGxoeD95hti0dTcLM",
     authDomain: "d1no-club.firebaseapp.com",
     databaseURL: "https://d1no-club.firebaseio.com",
@@ -33,11 +22,81 @@ function setup() {
     appId: "1:623680606310:web:579795658bc9ba9449dda7",
     measurementId: "G-SXZ489X6X9"
   };
-  console.log(firebase);
-  firebase.initializeApp(config);
+  firebase.initializeApp(firebaseConfig);
   database = firebase.database();
+   console.log(firebase);
+ 
+  /*canvas.mousePressed(startPath);
+  canvas.parent('canvascontainer');
+  canvas.mouseReleased(endPath);
 
-  var params = getURLParams();
+  var saveButton = select('#saveButton');
+  saveButton.mousePressed(saveDrawing);
+
+  var clearButton = select('#clearButton');
+  clearButton.mousePressed(clearDrawing);*/
+
+
+
+  
+  
+  
+}
+
+function startPath() {
+  isDrawing = true;
+  currentPath = [];
+  drawing.push(currentPath);
+}
+
+function endPath() {
+  isDrawing = false;
+}
+
+function draw() {
+  background(0);
+  if(isDrawing) {
+    var point = {
+      x: mouseX,
+      y:mouseY
+    }
+    currentPath.push(point);
+  }
+  
+  stroke(255);
+  strokeWeight(4);
+  noFill();
+  for (var i = 0; i < drawing.length; i++){
+    var path = drawing[i];
+    beginShape();
+    for (var j = 0; j < path.length; j++){
+      vertex(path[j].x,path[j].y)
+    }
+    endShape();
+    
+  }
+  
+}
+
+function saveDrawing(){
+  var ref= database.ref('drawings');
+  var data = {
+    name: "Doo",
+    drawing: drawing
+  }
+  var result = ref.push(data, dataSent);
+  console.log(result.key);
+  function dataSent(status) {
+    console.log(status);
+  }
+}
+
+// Initialize Firebase
+  
+
+
+
+  /*var params = getURLParams();
   console.log(params);
   if (params.id) {
     console.log(params.id);
@@ -151,4 +210,4 @@ function showDrawing(key) {
 
 function clearDrawing() {
   drawing = [];
-}
+}*/
